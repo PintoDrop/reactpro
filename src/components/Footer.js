e25b0448662886aceab6f8f9f4337f587642bcd6
PintoDrop <mikebender22@gmail.com>
2023-04-24 09:15:46 -0700

    adding linkedin and resume file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,10 @@ function Footer() {
       name: "Github",
       link: "https://github.com/PintoDrop",
     },
+    {
+      name: "LinkedIn",
+      link: "https://www.linkedin.com/in/michael-b-4612732b/",
+    },
   ];
 
   return (
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header(props) {
+  return (
+    <header className="flex-row">
+      <h1>Welcome to Michael's Portfolio!</h1>
+    </header>
+  );
+}
+
+
+
+export default Header;

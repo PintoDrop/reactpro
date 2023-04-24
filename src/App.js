0b5d69e7b76c336b5a8fc5e7c2016cd75abad31a
PintoDrop <mikebender22@gmail.com>
2023-04-24 09:24:54 -0700

    adding email validator

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+import "./App.css";
+
 
 // 
 function App() {
-  const [pages] = useState ([
-    {name: "about me" },
-    {name: "project"},
-    {name: "contact"}
-  ]);
-
-  const [currentPage, setCurrentPage] = useState(pages[0]);
 
   return (
-    <div>
-      <Header>
-        <Nav>
-          pages={pages}
-          setCurrentPage={setCurrentPage}
-          currentPage={currentPage}
-        </Nav>
-      </Header>
-      <main>
-
-      </main>
+    <div className="App">
+      <Header />
       <Footer />
     </div>
   );

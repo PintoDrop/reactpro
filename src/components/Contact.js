7b2c8927592938a7e628949ffad96d5217bd20e0
PintoDrop <mikebender22@gmail.com>
2023-04-24 11:30:50 -0700

    working on contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,19 @@ import React, {useState} from "react";
 import { validateEmail } from "../../utils/helper";
 
 function Contact() {
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
 
+  const [errorMessage, setErrorMessage] = useState('');
+  const [name, email, message] = formState;
+
+  const handleInputChange = (e) => {
+    e.preventDefault();
+    
+  }
 }
 
 
